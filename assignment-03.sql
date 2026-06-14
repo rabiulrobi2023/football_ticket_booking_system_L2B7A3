ab6af4d951c05c08a4c4bff511e4e81c34fa2301
MD RABIUL ISLAM <rabiul.robi2023@gmail.com>
2026-06-14 14:23:33 +0600

    User Table, Match Table, and Booking Table added

---
+++ assignment-03.sql
@@ -0,0 +1,41 @@
+-- ⚠️======================Delete Tables=====================
+DROP TABLE IF EXISTS Bookings;
+
+DROP TABLE IF EXISTS Matches;
+
+DROP TABLE IF EXISTS Users;
+
+-- ======================Create Tables======================
+CREATE TABLE Users (
+    user_id serial PRIMARY KEY,
+    full_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    role VARCHAR(30) CHECK (role IN ('Football Fan', 'Ticket Manager')) NOT NULL,
+    phone_number VARCHAR(14)
+);
+
+CREATE TABLE Matches (
+    match_id serial PRIMARY KEY,
+    fixture VARCHAR(50) NOT NULL,
+    tournament_category VARCHAR(50) NOT NULL,
+    base_ticket_price DECIMAL(10, 2) CHECK (base_ticket_price >= 0) NOT NULL,
+    match_status VARCHAR(30) NOT NULL CHECK (
+        match_status IN (
+            'Available',
+            'Selling Fast',
+            'Sold Out',
+            'Postponed'
+        )
+    )
+);
+
+CREATE TABLE Bookings (
+    booking_id serial PRIMARY KEY,
+    user_id INT REFERENCES users (user_id) NOT NULL,
+    match_id INT REFERENCES matches (match_id) NOT NULL,
+    seat_number VARCHAR(8),
+    payment_status VARCHAR(10) CHECK (
+        payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
+    ),
+    total_cost DECIMAL(10, 2) CHECK (total_cost >= 0) NOT NULL
+)
